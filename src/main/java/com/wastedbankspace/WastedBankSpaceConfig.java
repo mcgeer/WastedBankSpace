358c95c2a4056173feb0bb579898eb9de3904c35
Riley <Rileypatrickmcgee@gmail.com>
2021-06-19 00:54:27 -0400

    Testing inventory maps from thestonedturtle

--- src/main/java/com/wastedbankspace/WastedBankSpaceConfig.java
+++ src/main/java/com/wastedbankspace/WastedBankSpaceConfig.java
@@ -4,16 +4,65 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("Wasted Bank Space")
 public interface WastedBankSpaceConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+	/*Order
+		PoH Storage
+		Check Stash Units
+		Tackle Box
+		Bolt Pouch
+		-- Below here is for the future and Ideas--
+		Master Scroll Book
+		Key Ring
+		Seeds - Box or Vault
+		Herb Sack
+		Gem Bag
+		Rune Pouch
+		Fossil Island Chest
+
+		Other storage spaces
+		Looting bag
+		Hespori
+		Flag useless quest items
+	 */
+	@ConfigItem(keyName = "pohStorageCheck",
+			name = "PoH Storage",
+			description = "Check for items that could be in your PoH storage",
+			position = 1
 	)
-	default String greeting()
+	default boolean pohStorageCheck()
 	{
-		return "Hello";
+		return true;
+	}
+
+	@ConfigItem(keyName = "stashUnitCheck",
+			name = "Stash Units",
+			description = "Check for items that could be in unused Stash Units",
+			position = 2
+	)
+	default boolean stashUnitCheck()
+	{
+		return true;
+	}
+
+	@ConfigItem(keyName = "tackleBoxCheck",
+			name = "Tackle Box Items",
+			description = "Check for items that could be in your tackle box",
+			position = 3
+	)
+	default boolean tackleBoxCheck()
+	{
+		return true;
+	}
+
+	@ConfigItem(keyName = "boltPouchCheck",
+			name = "Bolt Pouch",
+			description = "Check for items that could be in your bolt pouch - Hirko in eastern Keldagrim",
+			position = 4
+	)
+	default boolean boltPouchCheck()
+	{
+		return true;
 	}
 }
--- src/main/java/com/wastedbankspace/WastedBankSpacePlugin.java
+++ src/main/java/com/wastedbankspace/WastedBankSpacePlugin.java
@@ -2,28 +2,52 @@ package com.wastedbankspace;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import javax.swing.SwingUtilities;
+
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Wasted Bank Space"
 )
+
 public class WastedBankSpacePlugin extends Plugin
 {
+	//=====Consts
+
+	//=====Inject
 	@Inject
 	private Client client;
 
 	@Inject
+	private ItemManager itemManager;
+
+	@Inject
 	private WastedBankSpaceConfig config;
 
+	//=====Provides
+	@Provides
+	WastedBankSpaceConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(WastedBankSpaceConfig.class);
+	}
+
+	//=====Local Properties
+	private final Map<Integer, Integer> inventoryHashMap = new HashMap<>();
+
+	//=====Override Runelite Functions
 	@Override
 	protected void startUp() throws Exception
 	{
@@ -36,18 +60,83 @@ public class WastedBankSpacePlugin extends Plugin
 		log.info("Example stopped!");
 	}
 
+	//=====Event Subscriptions
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			//client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.boltPouchCheck(), null);
 		}
 	}
 
-	@Provides
-	WastedBankSpaceConfig provideConfig(ConfigManager configManager)
+
+	/*
+	 * Adapted from thestonedturtle.bankedexperience;
+	 */
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged ev)
 	{
-		return configManager.getConfig(WastedBankSpaceConfig.class);
+		if (ev.getContainerId() == InventoryID.BANK.getId()
+				/*|| (ev.getContainerId() == InventoryID.SEED_VAULT.getId() && config.grabFromSeedVault())
+				|| (ev.getContainerId() == InventoryID.INVENTORY.getId() && config.grabFromInventory())
+				|| (ev.getContainerId() == LOOTING_BAG_ID && config.grabFromLootingBag())*/)
+		{
+			updateItemsFromItemContainer(ev.getContainerId(), ev.getItemContainer());
+		}
+	}
+
+	//=====Private Functions
+	private void updateItemsFromItemContainer(final int inventoryId, final ItemContainer c)
+	{
+		// Check if the contents have changed.
+		if (c == null)
+		{
+			return;
+		}
+
+		final Map<Integer, Integer> m = new HashMap<>();
+		for (Item item : c.getItems())
+		{
+			if (item.getId() == -1)
+			{
+				continue;
+			}
+
+			// Account for noted items, ignore placeholders.
+			int itemID = item.getId();
+			final ItemComposition itemComposition = itemManager.getItemComposition(itemID);
+			if (itemComposition.getPlaceholderTemplateId() != -1)
+			{
+				continue;
+			}
+
+			if (itemComposition.getNote() != -1)
+			{
+				itemID = itemComposition.getLinkedNoteId();
+			}
+
+			final int qty = m.getOrDefault(itemID, 0) + item.getQuantity();
+			m.put(itemID, qty);
+		}
+
+		updateInventoryMap(inventoryId, m);
+	}
+
+	private void updateInventoryMap(final int inventoryId, final Map<Integer, Integer> m)
+	{
+		final int curHash = m.hashCode();
+		if (curHash != inventoryHashMap.getOrDefault(inventoryId, -1))
+		{
+			inventoryHashMap.put(inventoryId, curHash);
+			log.debug("Inventory Id Added to Inventory Map: " + String.valueOf(inventoryId));
+			//SwingUtilities.invokeLater(() -> panel.setInventoryMap(inventoryId, m));
+		}
+
+		if(m.containsKey(314))
+		{
+			log.debug("FEATHER FOUND");
+		}
 	}
 }
